38ba1eb42ec828d6bcb504b1ac879c51e23f4005
Louis Orleans <louis@orleans.io>
2023-11-01 12:59:27 -0700

    🚧 WIP

--- dcs-kneeboard-generator/scripts/generate-pdfs.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactPDF from '@react-pdf/renderer';
-import { Kneeboard } from '../src';
-
-import { data } from '../src/data';
-
-ReactPDF.render(<Kneeboard kneeboard={data} />, `${__dirname}/example.pdf`);
